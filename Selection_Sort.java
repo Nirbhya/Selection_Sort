c9fbadb894c02012e4e92b827866e8f0ad021a64
Nirbhya Kaushik <53752594+Nirbhya@users.noreply.github.com>
2019-08-27 14:46:27 +0530

    Selection_Sort.java
    This source code is based on Selection Sort technique using Java language.
    Thank You !

---
+++ Selection_Sort.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Selection_Sort 
+{
+	public static void main(String args[])		      //Main Method
+	{
+		Scanner in=new Scanner(System.in);
+		int n,i,j,temp=0,min=0;					              // Variable Declaration
+		int arr[]=new int [50];					              // Array Declaration
+		System.out.println("Enter, How many elements do you want in array : ");
+		n=in.nextInt();							                  // Input of 'n' Variable
+//------------------------------------------------------------------
+		
+		for(i=0;i<n;i++)
+		{
+			arr[i]=in.nextInt();			                  	// Input Array Elements
+		}
+		
+//--------------------------------------------------------------------------
+
+		for(i=0;i<n-1;i++)
+		{
+			min=i;
+			for(j=i+1;j<n;j++)
+			{
+				if(arr[j]<arr[min])				                // Logic of Selection Sort
+				min=j;
+			}
+			temp=arr[i];
+			arr[i]=arr[min];				                  	// Swapping of array elements
+			arr[min]=temp;
+		}
+		
+//--------------------------------------------------------------------------------
+
+		for(i=0;i<n;i++)
+		{
+			System.out.println(arr[i]);				          //Output of Sorted Array
+		}
+
+	}
+}
