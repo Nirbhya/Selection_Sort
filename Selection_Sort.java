e83886a49990573de9e2d6cdd758a7887d0599e7
Nirbhya Kaushik <53752594+Nirbhya@users.noreply.github.com>
2019-08-28 11:38:34 +0530

    Update Selection_Sort.java

--- Selection_Sort.java
+++ Selection_Sort.java
@@ -14,6 +14,7 @@ public class Selection_Sort
 		{
 			arr[i]=in.nextInt();			                  	// Input Array Elements
 		}
+		in.close();
 		
 //--------------------------------------------------------------------------
 
